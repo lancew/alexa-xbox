8007df43b2ab349970e9b5ea7747a5d0ada3532a
Lance Wicks <lw@judocoach.com>
2018-01-12 21:35:22 +0000

    First pass using "standard" on the index.js
    Cleaned up a lot of trailing semi colons, can you guess I am a perl guy. ;-)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-const FauxMo = require('fauxmojs');
-const Xbox = require('xbox-on');
+const FauxMo = require('fauxmojs')
+const Xbox = require('xbox-on')
 
-const Server_ip    = 'xxx.xxx.xxx.xxx';
-const Xbox_ip      = 'xxx.xxx.xxx.xxx';
-const Xbox_live_id = 'FD00xxxxxxxxxxxx';
+const Server_ip = 'xxx.xxx.xxx.xxx'
+const Xbox_ip = 'xxx.xxx.xxx.xxx'
+const Xbox_live_id = 'FD00xxxxxxxxxxxx'
 
 let fauxMo = new FauxMo(
   {
@@ -15,20 +15,20 @@ let fauxMo = new FauxMo(
         name: 'xbox',
         port: 11000,
         handler: (action) => {
-            console.log('xbox action:', action);
-            var xbox = new Xbox(Xbox_ip, Xbox_live_id);
+          console.log('xbox action:', action)
+          var xbox = new Xbox(Xbox_ip, Xbox_live_id)
 
             // Optional, defaults shown
-            var options = {
-                tries: 5,
-                delay: 1000,
-                waitForCallback: false
-            };
+          var options = {
+            tries: 5,
+            delay: 1000,
+            waitForCallback: false
+          }
 
-            xbox.powerOn(options);
+          xbox.powerOn(options)
         }
-      },
+      }
     ]
-  });
+  })
 
-console.log('Xbox FauxMo server started...');
+console.log('Xbox FauxMo server started...')
